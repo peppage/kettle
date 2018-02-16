9ee7ae638db83890d5b4afe3a29a743544267687
Michael Peppler <peppage@gmail.com>
2018-02-15 21:10:13 -0500

    :bug: fix query param name of news feed

--- news.go
+++ news.go
@@ -45,7 +45,7 @@ type NewsItem struct {
 	Contents    string `json:"contents"`
 	FeedLabel   string `json:"feedlabel"`
 	Date        int64  `json:"date"`
-	FeedName    string `json:"feedname"`
+	FeedName    string `json:"feeds"`
 }
 
 // GetNewsForApp returns the latest of a game specified by its appID.
