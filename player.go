4805502e1c2075558273f22e3da6492272c1580e
Michael Peppler <peppage@gmail.com>
2019-02-13 08:45:05 -0500

    add GetRecentlyPlayedGames to IPlayerService

--- player.go
+++ player.go
@@ -45,6 +45,25 @@ type OwnedGamesParams struct {
 	//AppIdFilter too complicated right now
 }
 
+type playedGameResponse struct {
+	PlayedGameResponse pgresp `json:"response"`
+}
+
+type pgresp struct {
+	TotalCount int          `json:"total_count"`
+	Games      []PlayedGame `json:"games"`
+}
+
+// PlayedGame is a struct for a game from IPlayerService.GetRecentlyPlayedGames
+type PlayedGame struct {
+	AppID           int64  `json:"appid"`
+	Name            string `json:"name,omitempty"`
+	Playtime2Weeks  int    `json:"playtime_2weeks,omitempty"`
+	PlaytimeForever int    `json:"playtime_forever"`
+	ImgIconURL      string `json:"img_icon_url,omitempty"`
+	ImgLogoURL      string `json:"img_logo_url,omitempty"`
+}
+
 // GetOwnedGames returns a list of games a player owns along with some playtime information, if the profile is publicly visible.
 // If IncludeAppInfo is not set in the params then the response will only have
 // AppID, Playtime2Seeks, and PlaytimeForever
@@ -56,4 +75,22 @@ func (s IPlayerService) GetOwnedGames(params *OwnedGamesParams) ([]UserGame, *ht
 	resp, err := s.sling.New().Get("GetOwnedGames/v1/").QueryStruct(params).Receive(response, response)
 
 	return response.OwnedGameResponse.Games, resp, err
+}
+
+// RecentGamesParams are the parameters for IPlayerService.GetRecentlyPlayedGames
+type RecentGamesParams struct {
+	SteamID string `url:"steamid"`
+	Count   int    `url:"count,omitempty"`
+}
+
+// GetRecentlyPlayedGames returns a list of games a player has played in the last two weeks, if the profile is publicly visible.
+// Private, friends-only, and other privacy settings are not supported unless you are asking for your own personal details
+// (ie the WebAPI key you are using is linked to the steamid you are requesting).
+// https://developer.valvesoftware.com/wiki/Steam_Web_API#GetRecentlyPlayedGames_.28v0001.29
+func (s IPlayerService) GetRecentlyPlayedGames(params *RecentGamesParams) ([]PlayedGame, *http.Response, error) {
+	response := new(playedGameResponse)
+
+	resp, err := s.sling.New().Get("GetRecentlyPlayedGames/v0001/").QueryStruct(params).Receive(response, response)
+
+	return response.PlayedGameResponse.Games, resp, err
 }
--- player_test.go
+++ player_test.go
@@ -48,3 +48,42 @@ func TestIPlayerServiceGetOwnedGames(t *testing.T) {
 	assert.Equal(t, "e4ad9cf1b7dc8475c1118625daf9abd4bdcbcad0", games[0].ImgLogoURL)
 	assert.Equal(t, true, games[0].VisibleStats)
 }
+
+func TestIplayerServiceGetRecentlyPlayed(t *testing.T) {
+	t.Parallel()
+	const filePath = "./json/iplayerservice/recentlyplayedgames.json"
+	httpClient, mux, server := testServer()
+	defer server.Close()
+
+	mux.HandleFunc("/IPlayerService/GetRecentlyPlayedGames/v0001/", func(w http.ResponseWriter, r *http.Request) {
+		assertMethod(t, "GET", r)
+
+		assertQuery(t, map[string]string{
+			"steamid": "76561198006575550",
+			"key":     "",
+		}, r)
+
+		b, err := getTestFile(filePath)
+		if err != nil {
+			t.Fatalf("Failed to open testfile %s", filePath)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
+	})
+
+	client := NewClient(httpClient, "")
+	games, _, err := client.IPlayerService.GetRecentlyPlayedGames(&RecentGamesParams{
+		SteamID: "76561198006575550",
+	})
+
+	assert.Nil(t, err)
+	assert.True(t, len(games) > 0)
+
+	assert.Equal(t, int64(468670), games[0].AppID)
+	assert.Equal(t, "Speed Brawl", games[0].Name)
+	assert.Equal(t, 420, games[0].Playtime2Weeks)
+	assert.Equal(t, 421, games[0].PlaytimeForever)
+	assert.Equal(t, "8f48f2265746c08cd1fd7b8ce5f310172eb4fa12", games[0].ImgIconURL)
+	assert.Equal(t, "ba0d065302833a4851093410ced0e1c82df30ba4", games[0].ImgLogoURL)
+}
